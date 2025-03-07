28c1a988639a0eaa9d8f52dcb5921f28a4c5e705
devim <daywvidsonsgds@hotmail.com>
2025-03-07 20:27:27 -0300

    doação wordpressy

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html><html lang="pt-BR"><head>
+<title>Faça o bem ajudando a salvar vidas! | Vaquinhas Online</title>
+<meta name="description" content="Faça o bem ajudando a salvar vidas!">
+<meta name="keywords" content="Faça o bem ajudando a salvar vidas!">
+<meta name="author" content="Vakinha Online">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#FFFFFF">
+<meta name="apple-mobile-web-app-status-bar-style" content="#FFFFFF">
+<meta name="msapplication-navbutton-color" content="#FFFFFF">
+<meta property="og:title" content="Faça o bem ajudando a salvar vidas! | Vaquinhas Online">
+<meta property="og:description" content="Faça o bem ajudando a salvar vidas!">
+<meta property="og:url" content="#">
+<meta property="og:type" content="website">
+<meta property="og:image" content="images/img1.webp">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="icon" href="images/favicon.ico" sizes="32x32">
+<link rel="icon" href="images/favicon.ico" sizes="192x192">
+<link rel="apple-touch-icon" href="images/favicon.ico">
+<meta name="msapplication-TileImage" content="images/favicon.ico">
+
+<script src="<?= get_stylesheet_directory_uri(); ?>/jquery.min.js" defer></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/swiper-bundle.min.js" defer></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/bootstrap.min.js" defer></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/scripts.js" defer></script>
+
+<link rel="stylesheet" href="css/swiper-bundle.min.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<?php wp_head(); ?>
+</head>

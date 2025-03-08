f710ab732a6602a46e77f8ea07fc58e25b714a5c
devim <daywvidsonsgds@hotmail.com>
2025-03-08 14:56:28 -0300

    doação j

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 const swiper = new Swiper('.galeria', {
     direction: 'horizontal',
     loop: true,
-    pagination: { el: '.galeria-pagination' },
-    autoplay: { delay: 8000 },
+    pagination: {
+        el: '.galeria-pagination',
+    },
+    autoplay: {
+        delay: 8000,
+    },
 });
 
 const meta = 55000;
@@ -10,32 +14,66 @@ let arrecadado = 14589.00;
 let apoiadores = 82;
 let coracoes = 154;
 
+let themePath = "https://todospelajhuly.site/wp-content/themes/doacao";
+
 const arrayDados = [
-    { nome: "Doador anônimo", image: "https://todospelajhuly.site/wp-content/themes/doacao/images/icon.png", apoiador: 1, doado: 40, coracoes: 1 },
-    { nome: "Juliana Aparecida", image: "https://todospelajhuly.site/wp-content/themes/doacao/images/icon.png", apoiador: 1, doado: 30, coracoes: 0 },
-    { nome: "Lucas Fernandes", image: "https://todospelajhuly.site/wp-content/themes/doacao/images/icon.png", apoiador: 1, doado: 150, coracoes: 1 }
+    { nome: "Doador anônimo", apoiador: 1, doado: 40, coracoes: 1 },
+    { nome: "Juliana Aparecida", apoiador: 1, doado: 30, coracoes: 0 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 100, coracoes: 1 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 113.20, coracoes: 0 },
+    { nome: "Lucas Fernandes", apoiador: 1, doado: 150, coracoes: 1 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 100, coracoes: 1 },
+    { nome: "Fernanda Oliveira", apoiador: 1, doado: 200, coracoes: 0 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 50, coracoes: 1 },
+    { nome: "João Castro", apoiador: 1, doado: 40, coracoes: 0 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 100, coracoes: 1 },
+    { nome: "Marcela de Moraes", apoiador: 1, doado: 50, coracoes: 0 },
+    { nome: "Doador anônimo", apoiador: 1, doado: 150, coracoes: 1 },
 ];
 
 let index = 0;
 
 function atualizarValores() {
     if (index >= arrayDados.length) return;
-
+    
     let item = arrayDados[index];
     apoiadores += item.apoiador;
     coracoes += item.coracoes;
-
-    let novoValor = arrecadado + item.doado;
-    arrecadado = novoValor; // Atualiza primeiro
-
     animarValor("apoiadores", apoiadores);
     animarValor("coracoes", coracoes);
-    animarValor("doado", novoValor, arrecadado);
 
-    exibirNotificacao(item.nome, item.image, item.doado);
+    let novoValor = arrecadado + item.doado;
+    animarValor("doado", novoValor, arrecadado);
+    arrecadado = novoValor;
+    
+    exibirNotificacao(item.nome, item.doado);
     atualizarBarra();
     index++;
 }
+
+function atualizarBarra() {
+    let porcentagem = (arrecadado / meta) * 100;
+    let porcento = Math.round(porcentagem );
+    document.getElementById("barra").style.width = porcentagem + "%";
+    document.getElementById("barraMobile").style.width = porcentagem + "%";
+    document.getElementById("porcentagem").innerHTML = porcento + "%";
+}
+
+function exibirNotificacao(nome, valor) {
+    let notificacao = document.createElement("div");
+    notificacao.className = "notificacao";
+    notificacao.innerHTML = `<div class="content"><h4>${nome}</h4> Acabou de doar <strong>${formatarMoeda(valor)}</strong>.</div>`;
+    
+    document.body.appendChild(notificacao);
+
+    setTimeout(() => {
+        notificacao.style.transform = "translatey(0)";
+        notificacao.style.opacity = "0";
+        setTimeout(() => notificacao.remove(), 500);
+    }, 6000);
+}
+
+setInterval(atualizarValores, 30000);
 
 document.addEventListener("DOMContentLoaded", () => {
     atualizarBarra();
@@ -45,10 +83,23 @@ document.addEventListener("DOMContentLoaded", () => {
     document.head.appendChild(script);
 });
 
-let intervalo = setInterval(() => {
-    if (index < arrayDados.length) {
-        atualizarValores();
-    } else {
-        clearInterval(intervalo);
-    }
-}, 30000);
+jQuery(function($){
+    $(document).ready(function() {
+        $('a[href^="#"]').on('click', function(e) {
+          e.preventDefault();
+          var id = $(this).attr('href'),
+              targetOffset = $(id).offset().top;
+          $('html, body').animate({ 
+              scrollTop: targetOffset - 60
+          }, 1000);
+        });
+
+        $('.menu-mobile, .close-menu').click(function(){
+            $('.nav-mobile').toggleClass('active');
+        });
+    });
+
+    $('.btn-ajudar, .fora-modal, .close-modal').click(function(){
+        $('.modal-doar').toggleClass('open');
+    });
+});

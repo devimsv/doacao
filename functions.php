d8960c96544ed6f6139056995eca3a13847e926f
devim <daywvidsonsgds@hotmail.com>
2025-03-08 00:22:20 -0300

    doação jhuly

--- functions.php
+++ functions.php
@@ -1,10 +1,39 @@
 <?php
 
 function tigercodes_css() {
+  // Adiciona o estilo do tema
   wp_register_style('tigercodes-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
   wp_enqueue_style('tigercodes-style');
+
+  // Adiciona o Bootstrap CSS
+  wp_register_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', [], '5.0.0');
+  wp_enqueue_style('bootstrap-style');
+
+  // Adiciona o Swiper CSS
+  wp_register_style('swiper-style', get_template_directory_uri() . '/css/swiper-bundle.min.css', [], '6.0.0');
+  wp_enqueue_style('swiper-style');
 }
 add_action('wp_enqueue_scripts', 'tigercodes_css');
+
+
+function tigercodes_js() {
+  // Carregar o jQuery (muitas vezes já vem com o WordPress, então não é necessário se estiver sendo carregado automaticamente)
+  wp_enqueue_script('jquery'); // O jQuery já vem com o WordPress, você pode deixar isso para garantir que está sendo carregado
+  
+  // Carregar o Bootstrap JS
+  wp_register_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery'], '5.0.0', true); // Carregar após o jQuery
+  wp_enqueue_script('bootstrap-js');
+  
+  // Carregar o Swiper JS
+  wp_register_script('swiper-js', get_template_directory_uri() . '/js/swiper-bundle.min.js', [], '6.0.0', true); // Carregar no rodapé
+  wp_enqueue_script('swiper-js');
+  
+  // Carregar o seu arquivo de scripts personalizados
+  wp_register_script('custom-js', get_template_directory_uri() . '/js/scripts.js', [], '1.0.0', true); // Carregar no rodapé
+  wp_enqueue_script('custom-js');
+}
+add_action('wp_enqueue_scripts', 'tigercodes_js');
+
 
 
 

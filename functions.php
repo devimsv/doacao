09eb5b58b52ab9424eee1bed7727250382100e76
devim <daywvidsonsgds@hotmail.com>
2025-03-08 15:43:44 -0300

    doação wor

--- functions.php
+++ functions.php
@@ -1,25 +1,22 @@
-<?php
-
 function tigercodes_css() {
-  // Estilo principal
-  wp_register_style('tigercodes-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
-  wp_enqueue_style('tigercodes-style');
+    // Estilo principal
+    wp_register_style('tigercodes-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
+    wp_enqueue_style('tigercodes-style');
 
-  // Bootstrap CSS
-  wp_register_style('bootstrap-css', get_template_directory_uri() . '/bootstrap.min.css', [], '5.3.2');
-  wp_enqueue_style('bootstrap-css');
+    // Bootstrap CSS
+    wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', [], '5.3.2');
+    wp_enqueue_style('bootstrap-css');
 
-  // Swiper Bundle CSS
-  wp_register_style('swiper-css', get_template_directory_uri() . '/swiper-bundle.min.css', [], '10.3.1');
-  wp_enqueue_style('swiper-css');
+    // Swiper Bundle CSS
+    wp_register_style('swiper-css', get_template_directory_uri() . '/css/swiper-bundle.min.css', [], '10.3.1');
+    wp_enqueue_style('swiper-css');
 
-  // Scripts
-  wp_enqueue_script('jquery', get_template_directory_uri() . '/jquery.min.js', [], '3.7.1', true);
-  wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/bootstrap.min.js', ['jquery'], '5.3.2', true);
-  wp_enqueue_script('swiper-js', get_template_directory_uri() . '/swiper-bundle.min.js', [], '10.3.1', true);
-  wp_enqueue_script('scripts-js', get_template_directory_uri() . '/scripts.js', ['jquery'], '1.0.0', true);
+    // Scripts carregados após a DOM
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', [], '3.7.1', true);
+    wp_add_inline_script('jquery', 'document.addEventListener("DOMContentLoaded", function() { jQuery.noConflict(); });');
+
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery'], '5.3.2', true);
+    wp_enqueue_script('swiper-js', get_template_directory_uri() . '/js/swiper-bundle.min.js', ['jquery'], '10.3.1', true);
+    wp_enqueue_script('scripts-js', get_template_directory_uri() . '/js/scripts.js', ['jquery'], '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'tigercodes_css');
-
-
-?>

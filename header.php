b686007ce0e633c6cb2891ccff7d4950f9da8eed
devim <daywvidsonsgds@hotmail.com>
2025-03-08 16:09:55 -0300

    doação wordpr5

--- header.php
+++ header.php
@@ -1,6 +1,23 @@
 <?php $inicio_id = get_page_by_title('inicio')->ID; ?>
 
 <!DOCTYPE html><html lang="pt-BR"><head>
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1676973119888205');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1676973119888205&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
 <title>Faça o bem ajudando a salvar vidas! | Vaquinhas Online</title>
 <meta name="description" content="Faça o bem ajudando a salvar vidas!">
 <meta name="keywords" content="Faça o bem ajudando a salvar vidas!">

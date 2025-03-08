51b7b780f587c438dba1f4bcd351cb7635d7cac2
devim <daywvidsonsgds@hotmail.com>
2025-03-08 15:29:04 -0300

    doação wordpr

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@ function tigercodes_css() {
   wp_enqueue_style('tigercodes-style');
 
   // Bootstrap CSS
-  wp_register_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', [], '5.3.2');
+  wp_register_style('bootstrap-css', get_template_directory_uri() . '/bootstrap.min.css', [], '5.3.2');
   wp_enqueue_style('bootstrap-css');
 
   // Swiper Bundle CSS
-  wp_register_style('swiper-css', get_template_directory_uri() . '/css/swiper-bundle.min.css', [], '10.3.1');
+  wp_register_style('swiper-css', get_template_directory_uri() . '/swiper-bundle.min.css', [], '10.3.1');
   wp_enqueue_style('swiper-css');
 }
 add_action('wp_enqueue_scripts', 'tigercodes_css');
